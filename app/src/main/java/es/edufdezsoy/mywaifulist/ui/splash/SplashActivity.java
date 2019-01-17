2860971884af01bcf01dc9ff0267fe07163f60db
Eduardo Fernandez <yo@edufdezsoy.es>
2019-01-17 13:34:21 +0100

    mvp, work in progress

--- app/src/main/java/es/edufdezsoy/mywaifulist/ui/splash/SplashActivity.java
+++ app/src/main/java/es/edufdezsoy/mywaifulist/ui/splash/SplashActivity.java
@@ -5,11 +5,29 @@ import android.os.Bundle;
 
 import es.edufdezsoy.mywaifulist.R;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity implements SplashContract.View {
+    SplashContract.Presenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        presenter = new SplashPresenter(this);
+    }
+
+    @Override
+    public void showErrorBox(String msg) {
+
+    }
+
+    @Override
+    public void showMessageBox(String msg) {
+
+    }
+
+    @Override
+    public void onWaifuListLoaded() {
+
     }
 }
--- app/src/main/java/es/edufdezsoy/mywaifulist/ui/splash/SplashPresenter.java
+++ app/src/main/java/es/edufdezsoy/mywaifulist/ui/splash/SplashPresenter.java
@@ -1,4 +1,31 @@
 package es.edufdezsoy.mywaifulist.ui.splash;
 
-public class SplashPresenter {
+public class SplashPresenter implements SplashContract.Presenter, SplashInteractor.InteractorListener {
+    SplashContract.View view;
+    SplashInteractor interactor;
+
+    public SplashPresenter(SplashContract.View view) {
+        this.view = view;
+        interactor = new SplashInteractor();
+    }
+
+    @Override
+    public void loadWaifuList() {
+
+    }
+
+    @Override
+    public void onSuccess() {
+
+    }
+
+    @Override
+    public void onTimeOut() {
+
+    }
+
+    @Override
+    public void onConnectionError() {
+
+    }
 }
